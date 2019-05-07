e3f17c5a03b2000f59c4a06a68af481e36539872
Andreas Schloessl <andreas.sch4@gmail.com>
2019-05-07 16:11:45 +0200

    preview for first inputs added

--- scripts/auto-preview.js
+++ scripts/auto-preview.js
@@ -1,5 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("eventTitel").addEventListener('change', () => {
+    //Text inputs
+    [
+        ["#eventTitel", ".live-view .preview-title"],
+        ["#email-content", ".live-view .preview-opening"],
+    ].forEach((x) => {
+        document.querySelector(x[0]).addEventListener('change', () => {
+            document.querySelector(x[1]).innerHTML = document.querySelector(x[0]).value;
+        });
+    });
 
+    //Date - Time
+    [
+        ["#pickDate", "#pickTime", ".live-view .preview-date-begin"]
+    ].forEach((x) => {
+        document.querySelector(x[0]).addEventListener('change', () => {
+            document.querySelector(x[2]).innerHTML = new Date(document.querySelector(x[0]).value + " " + document.querySelector(x[1]).value).toLocaleString();
+        });
     });
 });
