1ad65d81cb88acaaf069590cbcbe87aa3019661b
MountainMechanics <6133@htl.rennweg.at>
2019-05-21 15:07:43 +0200

    [V0.4.0] Merged with Andreas, live view works now

---
+++ scripts/auto-preview.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+    //Text inputs
+    [
+        ["#eventTitel", ".live-view .preview-title"],
+        ["#event-details", ".live-view .preview-opening"],
+    ].forEach((x) => {
+        document.querySelector(x[0]).addEventListener('change', () => {
+            document.querySelector(x[1]).innerHTML = document.querySelector(x[0]).value;
+        });
+    });
+
+    //Date - Time
+    [
+        ["#pickDate", "#pickBeginTime",".live-view .preview-date-begin"],
+        ["#pickDate", "#pickEndTime",".live-view .preview-date-end"]
+    ].forEach((x) => {
+        [document.querySelector(x[0]), document.querySelector(x[1])].forEach(y => {
+            y.addEventListener('change', () => {
+                document.querySelector(x[2]).innerHTML = new Date(document.querySelector(x[0]).value + " " + document.querySelector(x[1]).value).toLocaleString();
+            });
+        });
+    });
+});
