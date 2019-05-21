a67a35ed6c2e30eea82ce826b8526e7485bfbd85
Andreas Schloessl <andreas.sch4@gmail.com>
2019-05-21 14:53:49 +0200

    standard value for fields added

--- scripts/auto-preview.js
+++ scripts/auto-preview.js
@@ -13,8 +13,10 @@ window.addEventListener('DOMContentLoaded', () => {
     [
         ["#pickDate", "#pickTime", ".live-view .preview-date-begin"]
     ].forEach((x) => {
-        document.querySelector(x[0]).addEventListener('change', () => {
-            document.querySelector(x[2]).innerHTML = new Date(document.querySelector(x[0]).value + " " + document.querySelector(x[1]).value).toLocaleString();
+        [document.querySelector(x[0]), document.querySelector(x[1])].forEach(y => {
+            y.addEventListener('change', () => {
+                document.querySelector(x[2]).innerHTML = new Date(document.querySelector(x[0]).value + " " + document.querySelector(x[1]).value).toLocaleString();
+            });
         });
     });
 });
